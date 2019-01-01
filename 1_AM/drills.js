8a163f8d85cf75df4734cc32a852d45588149df0
Jordan Perry <jordanperry@college.harvard.edu>
2019-01-01 16:45:34 -0500

    1 AM

---
+++ 1_AM/drills.js
@@ -0,0 +1,39 @@
+// Feel free to look things up online!
+
+const reverse = str => {
+  // TODO - write a function which reverses the string
+};
+
+const uniqueOnes = arr => {
+  // TODO - write a function which removes all non-unique elemenets from an array
+};
+
+const tokenize = str => {
+  // TODO - write a function which converts a multi-word string into an array of words
+};
+
+const factorial = num => {
+  // TODO - write a function which returns the factorial of a positive integer
+};
+
+const zip = (arr1, arr2) => {
+  // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
+  // if the two arrays are of unequal length
+  // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
+};
+
+const unzip = (arr1, arr2) => {
+  // TODO - Write a function which does the opposite of `zip()`
+};
+
+const shiftRight = (str, num) => {
+  // TODO - write a function which shifts a string `num` characters to the right
+  // Example = shiftThree("Hello") === "lloHe"
+};
+
+const announceDate = () => {
+  // TODO - write a function which returns the current date in the following format:
+  // "Today's date is January 7th, 2016. It is eleven thirty-seven in the morning."
+};
+
+// Write tests here:
