66ed54a82cb998c357bb4e18d91a28a339d06096
Jordan Perry <jordanperry@college.harvard.edu>
2019-01-01 17:31:33 -0500

    day 3 AM

---
+++ 3_AM/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/View"));
+app.use(express.static(__dirname + "/Script"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.listen(3000);
+console.log("Running at port 3000");
