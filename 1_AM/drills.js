1cc6fc0fecfc6c55fef1cfff5e11bae8e11b183b
Yooni Park <spencertiberi@gmail.com>
2019-01-07 12:53:18 -0500

    1 AM solutions

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -4,18 +4,36 @@ const assert = require("assert");
 
 const tokenize = str => {
   // TODO - write a function which converts a multi-word string into an array of words
+  const stringConvert = str.split(" ");
+
+  return stringConvert;
 };
 
 const reverse = str => {
   // TODO - write a function which reverses the string
+  const splitString = str.split("");
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join("");
+
+  return joinArray;
 };
 
 const uniqueOnes = arr => {
   // TODO - write a function which returns the inputted array without duplicate elements
+  let uniqueArray = Array.from(new Set(arr));
+
+  return uniqueArray;
 };
 
 const factorial = num => {
   // TODO - write a function which returns the factorial of a positive integer
+  if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 const zip = (arr1, arr2) => {
@@ -31,11 +49,35 @@ const unzip = arr => {
 const shiftRight = (str, num) => {
   // TODO - write a function which shifts a string `num` characters to the right
   // Example = shiftThree("Hello") === "lloHe"
+  const splitString = str.split();
 };
 
 const announceDate = () => {
   // TODO - write a function which returns the current date in the following format:
   // "Today's date is January 7th, 2016. It is eleven thirty-seven in the morning."
+  const currentDate = new Date();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const date = currentDate.getDate();
+  const month = (document.getElementById("demo").innerHTML =
+    months[currentDate.getMonth()]);
+  const year = currentDate.getFullYear();
+
+  console.log(`Today\'s date is ${month} ${date}, ${year}.`);
 };
 
 // Write tests here:
@@ -43,4 +85,4 @@ const announceDate = () => {
 assert(1 < 2);
 assert(1 + 2 === 3);
 assert([2, 3][0] === 2);
-// asssert (reverse("3df") === "fd3")
+assert(reverse("3df") === "fd3");
